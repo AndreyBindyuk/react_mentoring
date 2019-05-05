66f95b62deaf160f210bed06b0ebbebbbd8c50aa
Andrey <anbind@mail.ru>
2019-05-05 21:06:18 +0600

    initial state was set for search component

--- src/searchImpl/SearchContainer.js
+++ src/searchImpl/SearchContainer.js
@@ -3,19 +3,33 @@ import "../css/Search.css";
 import mainLogo from '../movie_pic.png';
 
 class SearchContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            red: true
+        }
+    }
+
+    changeColor() {
+        this.setState({ red: !this.state.red })
+    }
     render() {
-        console.log(mainLogo);
+        
+        let btn_title_class = this.state.red ? "btn_red" : "";
+        let btn_genre_class = this.state.red ? "" : "btn_red";
         return (
             <div className="search-container">
-                {/* <div className="logo-container"> */}
-                <img className="logo-container" src={mainLogo}/>
-                {/* </div> */}
+                <img className="logo-container" src={mainLogo} />
                 <div className="input-button-container">
                     <input className="input-container" type="input" />
                     <div className="buttons-container">
                         <div className="title-genre-button-container">
-                            <button><span>TITLE</span></button>
-                            <button><span>GENRE</span></button>
+                            <button id="btn_title" className={btn_title_class} onClick={this.changeColor.bind(this)}>
+                                <span>TITLE</span>
+                            </button>
+                            <button id="btn_genre" className={btn_genre_class} onClick={this.changeColor.bind(this)}>
+                                <span>GENRE</span>
+                            </button>
                         </div>
                         <button><span>SEARCH</span></button>
                     </div>
