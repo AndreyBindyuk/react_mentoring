eba9d1c843d8edc48ff7ea6378cbbb6147053d92
Andrey Bindyuk <Andrey_Bindyuk@epam.com>
2019-05-06 23:08:48 +0600

    logic for initial btn state has been implemented

--- src/searchImpl/SearchContainer.js
+++ src/searchImpl/SearchContainer.js
@@ -10,13 +10,18 @@ class SearchContainer extends React.Component {
         }
     }
 
-    changeColor() {
-        this.setState({ red: !this.state.red })
+    changeColor(params) {
+        if(document.getElementById(params).style.backgroundColor == "red"){
+            return
+        } else{
+            this.setState({ red: !this.state.red })
+        }
     }
+
     render() {
         
-        let btn_title_class = this.state.red ? "btn_red" : "";
-        let btn_genre_class = this.state.red ? "" : "btn_red";
+        let btn_title = this.state.red ? "red" : "";
+        let btn_genre = this.state.red ? "" : "red";
         return (
             <div className="search-container">
                 <img className="logo-container" src={mainLogo} />
@@ -24,10 +29,10 @@ class SearchContainer extends React.Component {
                     <input className="input-container" type="input" />
                     <div className="buttons-container">
                         <div className="title-genre-button-container">
-                            <button id="btn_title" className={btn_title_class} onClick={this.changeColor.bind(this)}>
+                            <button id="btn_title" onClick={this.changeColor.bind(this,"btn_title")} style={{backgroundColor: btn_title}}>
                                 <span>TITLE</span>
                             </button>
-                            <button id="btn_genre" className={btn_genre_class} onClick={this.changeColor.bind(this)}>
+                            <button id="btn_genre" onClick={this.changeColor.bind(this,"btn_genre")} style={{backgroundColor: btn_genre}}>
                                 <span>GENRE</span>
                             </button>
                         </div>
@@ -36,6 +41,26 @@ class SearchContainer extends React.Component {
                 </div>
             </div>
         );
+    }
+}
+
+class Button extends React.Component {   
+    constructor(){
+    super(); 
+    this.state = {
+        color_black: true,
+      }
+    }
+    changeColor(){
+            this.setState({color_black: !this.state.color_black})
+    }
+        render(){
+        let bgColor = this.state.color_black ? this.props.color : this.props.color2
+        return (
+        <div>
+            <button style={{backgroundColor: bgColor}} onClick={this.changeColor.bind(this)}>Button1</button>
+        </div>
+      )
     }
 }
 
