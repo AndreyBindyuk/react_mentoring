544d497730d2e9ad22a5d947f4185203f9e8cc13
Andrey <anbind@mail.ru>
2019-05-07 17:39:48 +0600

    search container adjustments

--- src/searchImpl/SearchContainer.js
+++ src/searchImpl/SearchContainer.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import "../css/Search.css";
 import mainLogo from '../movie_pic.png';
+import MovieStub from '../movieItemImpl/MovieStub.json'
 
 class SearchContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            red: true
+            btn_title_genre: true,
+            btn_release_rating: true
         }
     }
 
-    changeColor(params) {
-        if(document.getElementById(params).style.backgroundColor == "red"){
-            return
-        } else{
-            this.setState({ red: !this.state.red })
+    changeBtnColor(params) {
+        if ((params =="btn_title" || params =="btn_genre") && document.getElementById(params).style.color != "red"){
+            this.setState({ btn_title_genre: !this.state.btn_title_genre })
+        } else if((params =="btn_release" || params =="btn_rating") && document.getElementById(params).style.color != "red"){
+            this.setState({ btn_release_rating: !this.state.btn_release_rating })
         }
     }
 
     render() {
-        
-        let btn_title = this.state.red ? "red" : "";
-        let btn_genre = this.state.red ? "" : "red";
+        let red_btn = this.state.btn_title_genre ? "red" : "";
+        let gray_btn = this.state.btn_title_genre ? "" : "red";
+        let r_red_btn = this.state.btn_release_rating ? "red" : "";
+        let r_gray_btn = this.state.btn_release_rating ? "" : "red";
+
         return (
             <div className="search-container">
                 <img className="logo-container" src={mainLogo} />
@@ -29,38 +33,25 @@ class SearchContainer extends React.Component {
                     <input className="input-container" type="input" />
                     <div className="buttons-container">
                         <div className="title-genre-button-container">
-                            <button id="btn_title" onClick={this.changeColor.bind(this,"btn_title")} style={{backgroundColor: btn_title}}>
+                            <button id="btn_title" onClick={this.changeBtnColor.bind(this, "btn_title")} style={{ color: red_btn }}>
                                 <span>TITLE</span>
                             </button>
-                            <button id="btn_genre" onClick={this.changeColor.bind(this,"btn_genre")} style={{backgroundColor: btn_genre}}>
+                            <button id="btn_genre" onClick={this.changeBtnColor.bind(this, "btn_genre")} style={{ color: gray_btn }}>
                                 <span>GENRE</span>
                             </button>
                         </div>
                         <button><span>SEARCH</span></button>
                     </div>
+                    <div className="movies-search">
+                        <h5>{MovieStub.length} movies found</h5>
+                        <div className="release-rating">
+                            <button id="btn_release" className="btn" style={{ color: r_red_btn }} onClick={this.changeBtnColor.bind(this, "btn_release")}>release date</button>
+                            <button id="btn_rating" className="btn" style={{ color: r_gray_btn }} onClick={this.changeBtnColor.bind(this, "btn_rating")}>rating</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
-    }
-}
-
-class Button extends React.Component {   
-    constructor(){
-    super(); 
-    this.state = {
-        color_black: true,
-      }
-    }
-    changeColor(){
-            this.setState({color_black: !this.state.color_black})
-    }
-        render(){
-        let bgColor = this.state.color_black ? this.props.color : this.props.color2
-        return (
-        <div>
-            <button style={{backgroundColor: bgColor}} onClick={this.changeColor.bind(this)}>Button1</button>
-        </div>
-      )
     }
 }
 
