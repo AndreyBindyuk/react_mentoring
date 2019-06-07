750b94d1e69aece70059834e0acf96c7233d127d
Andrey Bindyuk <Andrey_Bindyuk@epam.com>
2019-06-07 10:40:54 +0600

    MovieSingleView test was added

---
+++ src/containers/movieSingleView/MovieSingleView.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import MovieSingleView from "./MovieSingleView";
+
+describe("<MovieSingleView />", () => {
+    it("should render components correctly", () => {
+      const component = shallow(<MovieSingleView />);
+      expect(component).toMatchSnapshot();
+    });
+  });
